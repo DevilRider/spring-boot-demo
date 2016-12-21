7db8942e702a45fc097ac04be2a18f34433da9dc
L.Yang <lyang@tsingsoft.com.cn>
2016-12-21 15:44:45 +0800

    Initial commit

---
+++ src/main/java/org/liang/web/HelloController.java
@@ -0,0 +1,28 @@
+package org.liang.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * hello world cntroller
+ * Created by La on 2016/12/21.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://www.baidu.com");
+        return "index";
+    }
+
+}
