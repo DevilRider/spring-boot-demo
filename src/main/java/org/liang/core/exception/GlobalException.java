d8910aa962e36b4abf07daed4ea290497306cae8
L.Yang <lyang@tsingsoft.com.cn>
2017-01-04 09:30:33 +0800

    修正代码 解决警告

--- src/main/java/org/liang/core/exception/GlobalException.java
+++ src/main/java/org/liang/core/exception/GlobalException.java
@@ -2,6 +2,8 @@ package org.liang.core.exception;
 
 public class GlobalException extends Exception {
 
+    private static final long serialVersionUID = 8030755163597720379L;
+
     public GlobalException(String message) {
         super(message);
     }
